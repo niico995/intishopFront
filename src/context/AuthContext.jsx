7bce85422fb516220936c21f3f56bb3078f1d824
Fernando Juarez <niicoo995@gmail.com>
2025-09-06 08:58:40 -0300

    auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,12 @@
 import React, { createContext, useContext } from "react";
+
 const AuthCtx = createContext({ usuario: null });
 export const useAuth = () => useContext(AuthCtx);
+
 export function AuthProvider({ children }) {
-  return <AuthCtx.Provider value={{ usuario: null }}>{children}</AuthProvider>;
+  return (
+    <AuthCtx.Provider value={{ usuario: null }}>
+      {children}
+    </AuthCtx.Provider>
+  );
 }
